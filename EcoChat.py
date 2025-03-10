61d0772424adcdeaad93e2e1b6fb410b552e63ad
Parantap-2023 <parantapmishra2023@gmail.com>
2025-03-10 22:53:27 +0530

    Update EcoChat.py

--- EcoChat.py
+++ EcoChat.py
@@ -4,7 +4,7 @@ import pandas as pd
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain
-from langchain_chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import time
 from langchain_huggingface import HuggingFaceEmbeddings
